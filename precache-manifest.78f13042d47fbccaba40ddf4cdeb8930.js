a13ee588439ca75c1b0aeea1a456ed2397a5cef7
Damian Żmuda <d.zmuda94@gmail.com>
2019-08-08 19:35:01 +0200

    Updates

---
+++ precache-manifest.78f13042d47fbccaba40ddf4cdeb8930.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6931c550a9019d50af69c503e0d6c48d",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "6bc2fe701b5e2368e051",
+    "url": "/Weather-app/static/js/2.79ae044f.chunk.js"
+  },
+  {
+    "revision": "6088a05f13f5a83bc858",
+    "url": "/Weather-app/static/js/main.c25b0802.chunk.js"
+  },
+  {
+    "revision": "cd23450c59ffd8f53315",
+    "url": "/Weather-app/static/js/runtime~main.9b6b42b8.js"
+  }
+]);
